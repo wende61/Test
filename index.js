8be37032ddc8da0f2f6d77a024ced85f48a6dac9
wende61 <wendebuzu@gmail.com>
2022-05-02 20:44:54 +0300

    fix mongo db and added manage session

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const session = require('express-session');
+//const session = require('cookie-session');
 dotenv.config();
 const passport = require("passport");
 const { loginCheck } = require("./auth/passport");
@@ -20,32 +21,32 @@ mongoose
 app.set("view engine", "ejs");
 
 //BodyParsing
-// app.use(express.urlencoded({ extended: false }));
-// app.use(session({
-//     secret:'oneboy',
-//     saveUninitialized: true,
-//     resave: true
-//   }));
-//-momery unleaked---------
-app.set('trust proxy', 1);
-
+app.use(express.urlencoded({ extended: false }));
 app.use(session({
-cookie:{
-    secure: true,
-    maxAge:60000
-       },
-    store: new RedisStore(),
-    secret: 'secret',
+    secret:'oneboy',
     saveUninitialized: true,
-    resave: false
-}));
+    resave: true
+  }));
+//-momery unleaked---------
+// app.set('trust proxy', 1);
 
-app.use(function(req,res,next){
-if(!req.session){
-    return next(new Error('Oh no')) //handle error
-}
-next() //otherwise continue
-});
+// app.use(session({
+// cookie:{
+//     secure: true,
+//     maxAge:60000
+//        },
+//     store: new RedisStore(),
+//     secret: 'secret',
+//     saveUninitialized: true,
+//     resave: false
+// }));
+
+// app.use(function(req,res,next){
+// if(!req.session){
+//     return next(new Error('Oh no')) //handle error
+// }
+// next() //otherwise continue
+// });
 app.use(passport.initialize());
 app.use(passport.session());
 

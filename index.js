5f0e6060fae8725f0e74dcb3e249448359449472
wende61 <wendebuzu@gmail.com>
2022-05-02 20:28:42 +0300

    added memory leak for mongoos

--- index.js
+++ index.js
@@ -20,13 +20,32 @@ mongoose
 app.set("view engine", "ejs");
 
 //BodyParsing
-app.use(express.urlencoded({ extended: false }));
-app.use(session({
-    secret:'oneboy',
-    saveUninitialized: true,
-    resave: true
-  }));
+// app.use(express.urlencoded({ extended: false }));
+// app.use(session({
+//     secret:'oneboy',
+//     saveUninitialized: true,
+//     resave: true
+//   }));
+//-momery unleaked---------
+app.set('trust proxy', 1);
 
+app.use(session({
+cookie:{
+    secure: true,
+    maxAge:60000
+       },
+    store: new RedisStore(),
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: false
+}));
+
+app.use(function(req,res,next){
+if(!req.session){
+    return next(new Error('Oh no')) //handle error
+}
+next() //otherwise continue
+});
 app.use(passport.initialize());
 app.use(passport.session());
 
